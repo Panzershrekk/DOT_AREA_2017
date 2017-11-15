cc61cd328c3dde5c07f11e0de8e92edfb2ad3e1e
Thomas Fossaert <thomas.fossaert@epitech.eu>
2017-11-15 15:54:31 +0100

    [+] [Module Steam] Ajout du module steam, récupération liste de jeux

---
+++ Api/Controllers/SteamController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class SteamController : Controller
+    {
+        private Module.ModuleSteam module { get; set; }
+
+        public SteamController()
+        {
+            module = new Module.ModuleSteam();
+        }
+
+        [HttpGet]
+        public string Index()
+        {
+            return module.GetRequest();
+        }
+
+        /*
+        [HttpGet("toto/{id}")]
+        public string Get(int id)
+        {
+            return $"toto-id = {id}";
+        }
+        */
+    }
+}
---
+++ ModuleSteam/Steam.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using JsonApiSerializer;
+using Module;
+using Newtonsoft.Json;
+using PortableSteam;
+using PortableSteam.Fluent.General.ISteamUser;
+using PortableSteam.Infrastructure.Converters;
+using PortableSteam.Interfaces.General.ISteamUser;
+
+namespace Module
+{
+    public class ModuleSteam : AModule
+    {
+        public override string GetRequest()
+        {
+            //535AB6031548709DFEEB34CABB80601E
+            SteamWebAPI.SetGlobalKey("535AB6031548709DFEEB34CABB80601E");
+            var response = SteamWebAPI.General().IPlayerService()
+                .GetOwnedGames(SteamIdentity.FromAccountID(76561197998335096)).GetResponseString();
+            /*.Game().Generic()*/
+                /*.IEconItems(GameType.Portal2)
+                .GetPlayerItems(SteamIdentity.FromSteamID(76561197998335096))
+                .GetResponse();*/
+
+            return (response);
+            //return JsonConvert.SerializeObject(response, new JsonApiSerializerSettings());
+        }
+
+        public override string PostRequest()
+        {
+            return JsonConvert.SerializeObject("", new JsonApiSerializerSettings());
+        }
+    }
+}
