db3294799c71b075582412f9a18414284b003509
CAUCHOIS Guillaume <guillaume.cauchois@epitech.eu>
2017-11-15 20:39:37 +0100

    [+] DAO Initialisation of the database

---
+++ Api/Area.cs
@@ -0,0 +1,31 @@
+﻿using DAO;
+
+namespace Api
+{
+    public class Area
+    {
+        private Database Database { get; set; }
+
+        private void InitDatabase()
+        {
+            var daoFactory = new DaoFactory("database.properties");
+            Database = daoFactory.GetInstance();
+
+            const string initTableRequest = "CREATE TABLE IF NOT EXISTS `user` (" +
+                "`id` int(11) NOT NULL AUTO_INCREMENT," +
+                "`username` varchar(255) DEFAULT NULL," +
+                "`password` varchar(255) DEFAULT NULL," +
+                "`firstname` varchar(255) DEFAULT NULL," +
+                "`lastname` varchar(255) DEFAULT NULL," +
+                "PRIMARY KEY (`id`)" +
+                ") ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;";
+            
+            Database.Execute(initTableRequest);
+        }
+        
+        public void Init()
+        {
+            InitDatabase();
+        }
+    }
+}
