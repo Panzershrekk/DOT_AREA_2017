85f83c90fdcf1061689f4d4dcefa167a511b1c28
Unknown <adrien.warin@epitech.eu>
2017-11-16 16:07:07 +0100

    [*] [Gmail api] create UnitTest
    create UnitTest and remove comment

---
+++ UnitTests/GmailTest.cs
@@ -0,0 +1,17 @@
+﻿using Module;
+using Xunit;
+
+namespace UnitTests
+{
+    public class GmailTest
+    {
+        private Module.GmailModule module { get; set; }
+        [Fact]
+        public void TestSendMessage()
+        {
+            module = new GmailModule();
+            string test = module.SendMessage("grattepanche.robin@gmail.com", "test", "hello world!");
+            Assert.Equal(test, "OK");
+        }
+    }
+}
